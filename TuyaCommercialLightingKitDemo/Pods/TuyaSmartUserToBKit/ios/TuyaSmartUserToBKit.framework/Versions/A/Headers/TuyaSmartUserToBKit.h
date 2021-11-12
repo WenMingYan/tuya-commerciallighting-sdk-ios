fdb2ad02cd4668ec4fe3efd33a0c6b49aa1c131b
lingchen <lingchen@tuya.com>
2021-11-12 16:08:16 +0800

    Modify SDK name

---
+++ TuyaCommercialLightingKitDemo/Pods/TuyaSmartUserToBKit/ios/TuyaSmartUserToBKit.framework/Versions/A/Headers/TuyaSmartUserToBKit.h
@@ -0,0 +1,16 @@
+//
+//  TuyaSmartUserToBKit.h
+//  TuyaSmartUserToBKit
+//
+//  Created by huxn on 2021/5/26.
+//
+
+#ifndef TuyaSmartUserToBKit_h
+#define TuyaSmartUserToBKit_h
+
+#import <TuyaSmartBaseKit/TuyaSmartBaseKit.h>
+
+#import "TuyaSmartUser+Merchant.h"
+#import "TuyaSmartMerchantModel.h"
+
+#endif /* TuyaSmartUserToBKit_h */
